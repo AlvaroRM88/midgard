891248fba03b9b05f5b9282bb0600a0fd4f0dfef
AlvaroRM88 <arm071088@gmail.com>
2018-11-04 16:37:07 +0100

    First commit

---
+++ src/test/java/com/codecraftsmen/midgard/MidgardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecraftsmen.midgard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MidgardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
